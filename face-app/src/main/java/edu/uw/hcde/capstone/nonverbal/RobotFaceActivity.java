70f04a1ad6cce02ecc4e8735a3cc6f545c923c3d
John <john@johnluetke.net>
2017-07-04 13:38:14 -0700

    Refactor RobotFaceActivity to utilize external BT classes

--- face-app/src/main/java/edu/uw/hcde/capstone/nonverbal/RobotFaceActivity.java
+++ face-app/src/main/java/edu/uw/hcde/capstone/nonverbal/RobotFaceActivity.java
@@ -2,23 +2,24 @@ package edu.uw.hcde.capstone.nonverbal;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.media.MediaPlayer;
 import android.net.Uri;
-import android.nfc.tech.NfcBarcode;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.VideoView;
 
-import java.io.IOException;
-import java.io.InputStream;
+import com.simthesocialrobot.app.Message;
+import com.simthesocialrobot.app.android.bluetooth.BluetoothConnectThread;
+import com.simthesocialrobot.app.android.bluetooth.BluetoothConnectionHandler;
+import com.simthesocialrobot.app.android.bluetooth.BluetoothIncomingMessageThread;
+import com.simthesocialrobot.app.android.bluetooth.BluetoothIncomingMessageHandler;
+
 import java.util.Random;
-import java.util.UUID;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -36,8 +37,8 @@ public class RobotFaceActivity extends Activity {
     int numIdleExpressions;
 
     BluetoothAdapter btAdapter;
-    BTConnectThread btConnectThread;
-    BTMessageThread btMessageThread;
+    BluetoothConnectThread btConnectThread;
+    BluetoothIncomingMessageThread btIncomingMessageThread;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -45,13 +46,61 @@ public class RobotFaceActivity extends Activity {
 
         btAdapter = BluetoothAdapter.getDefaultAdapter();
 
+        btConnectThread = new BluetoothConnectThread(btAdapter, new BluetoothConnectionHandler() {
+            @Override
+            public void onBluetoothSocketAvailable(BluetoothSocket socket) {
+                if (socket == null) {
+                    setResult(MainActivity.BT_CONNECTION_TIMEOUT);
+                    finish();
+                }
+                else {
+                    nextVideoUri = getResourceUri(R.raw.happy_s01);
+
+                    btIncomingMessageThread = new BluetoothIncomingMessageThread(socket, new BluetoothIncomingMessageHandler() {
+                        @Override
+                        public void onInputStreamDisconnected() {
+                            finish();
+                        }
+
+                        @Override
+                        public void onMessageRecieved(Message message) {
+                            if (message == null) {
+                                return;
+                            }
+
+                            if (message.equals(Message.SLEEP)) {
+                                robotMode = RobotMode.SLEEP;
+                            }
+                            else {
+                                robotMode = RobotMode.IDLE;
+                            }
+
+                            if (message.equals(Message.SWITCH)) {
+                                robotType = robotType == RobotType.DUMBOT ? RobotType.SIM : RobotType.DUMBOT;
+                                videos = loadVideos();
+                                numIdleExpressions = getNumIdleExpressions();
+                            }
+
+                            for (int i = 0; i < videos.length(); i++) {
+                                String item = videos.getString(i);
+                                if (item.toLowerCase().contains(message.getMessageString().toLowerCase())) {
+                                    nextVideoUri = getResourceUri(videos.getResourceId(i, 0));
+                                    break;
+                                }
+                            }
+                        }
+                    });
+
+                    btIncomingMessageThread.start();
+                }
+            }
+        });
+        btConnectThread.start();
+
         Intent intent = getIntent();
         robotType = RobotType.valueOf(intent.getStringExtra(MainActivity.ROBOT_TYPE));
         robotMode = RobotMode.IDLE;
         numIdleExpressions = getNumIdleExpressions();
-
-        btConnectThread = new BTConnectThread();
-        btConnectThread.start();
 
         setContentView(R.layout.activity_robot_face);
         videoView = (VideoView) findViewById(R.id.video_view);
@@ -140,171 +189,4 @@ public class RobotFaceActivity extends Activity {
             getResources().obtainTypedArray(R.array.dumbotVideos) :
             getResources().obtainTypedArray(R.array.simVideos);
     }
-
-    /**
-     * THIS RUNS ON ANOTHER THREAD! CANNOT CHANGE THE VIDEO VIEW DIRECTLY
-     *
-     * @param socket
-     */
-    private void onBluetoothSocketAvailable(BluetoothSocket socket) {
-        if (socket == null) {
-            setResult(MainActivity.BT_CONNECTION_TIMEOUT);
-            finish();
-        }
-        else {
-            nextVideoUri = getResourceUri(R.raw.happy_s01);
-            btMessageThread = new BTMessageThread(socket);
-            btMessageThread.start();
-        }
-    }
-
-    /**
-     * THIS RUNS ON ANOTHER THREAD! CANNOT CHANGE THE VIDEO VIEW DIRECTLY
-     *
-     * @param message
-     */
-    private void processMessage(String message) {
-        if (message == null) {
-            return;
-        }
-
-        if (message.contains("sleep")) {
-            robotMode = RobotMode.SLEEP;
-        }
-        else {
-            robotMode = RobotMode.IDLE;
-        }
-
-        if (message.contains("switch")) {
-            robotType = robotType == RobotType.DUMBOT ? RobotType.SIM : RobotType.DUMBOT;
-            videos = loadVideos();
-            numIdleExpressions = getNumIdleExpressions();
-        }
-
-        setNextVideoFromMessage(message);
-    }
-
-    /**
-     * THIS RUNS ON ANOTHER THREAD! CANNOT CHANGE THE VIDEO VIEW DIRECTLY
-     *
-     * @param message
-     */
-    public Uri setNextVideoFromMessage(String message) {
-        for (int i = 0; i < videos.length(); i++) {
-            String item = videos.getString(i);
-            if (item.toLowerCase().contains(message.toLowerCase())) {
-                nextVideoUri = getResourceUri(videos.getResourceId(i, 0));
-                break;
-            }
-        }
-
-        return nextVideoUri;
-    }
-
-    private class BTConnectThread extends Thread {
-        private static final String TAG = MainActivity.BLUETOOTH_SERVICE_NAME;
-        private static final int BT_TIMEOUT = 15000;
-
-        private final BluetoothServerSocket serverSocket;
-
-        public BTConnectThread() {
-            BluetoothServerSocket tmpServerSocket = null;
-            try {
-                tmpServerSocket = btAdapter.listenUsingInsecureRfcommWithServiceRecord(MainActivity.BLUETOOTH_SERVICE_NAME, UUID.fromString(MainActivity.BLUETOOTH_SERVICE_UUID));
-            }
-            catch (IOException e) {
-                Log.e(TAG, "Failed to listen for connection", e);
-            }
-
-            serverSocket = tmpServerSocket;
-        }
-
-        public void run() {
-            BluetoothSocket socket = null;
-            while (true) {
-                try {
-                    socket = serverSocket.accept(BT_TIMEOUT);
-                    onBluetoothSocketAvailable(socket);
-                }
-                catch (IOException e) {
-                    Log.e(TAG, "Server Socket's accept() method failed", e);
-                    break;
-                }
-                if (socket != null) {
-                    try {
-                        serverSocket.close();
-                        break;
-                    } catch (IOException e) {
-                        Log.e(TAG, "Server Socket's close() method failed", e);
-                        break;
-                    }
-                }
-            }
-
-            if (socket == null) {
-                onBluetoothSocketAvailable(null);
-            }
-        }
-
-        public void cancel() {
-            try {
-                serverSocket.close();
-            }
-            catch (IOException e) {
-                Log.e(TAG, "Could not close the connect socket", e);
-            }
-        }
-    }
-
-    private class BTMessageThread extends Thread {
-        private static final String TAG = MainActivity.BLUETOOTH_SERVICE_NAME;
-
-        private final BluetoothSocket socket;
-        private final InputStream inputStream;
-        private byte[] buffer;
-
-        public BTMessageThread(BluetoothSocket socket) {
-            this.socket = socket;
-
-            InputStream tmpInputStream = null;
-
-            try {
-                tmpInputStream = socket.getInputStream();
-            } catch (IOException e) {
-                Log.e(TAG, "Error occurred when creating input stream", e);
-            }
-
-            inputStream = tmpInputStream;
-        }
-
-        public void run() {
-            buffer = new byte[1024];
-            int numBytes;
-
-            while (true) {
-                try {
-                    numBytes = inputStream.read(buffer);
-                    Log.d(TAG, String.format("Got message: %s", new String(buffer).substring(0, numBytes)));
-                    processMessage(new String(buffer, 0, numBytes));
-                }
-                catch (IOException e) {
-                    Log.e(TAG, "Input stream was disconnected", e);
-                    finish();
-                    break;
-                }
-                catch (NullPointerException e) {
-                    setNextVideoFromMessage("idle");
-                }
-            }
-        }
-
-        public void cancel() {
-            try {
-                socket.close();
-            } catch (IOException e) {
-                Log.e(TAG, "Could not close the connect socket", e);
-            }
-        }
-    }
-
 }
